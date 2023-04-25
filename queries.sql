7c8243a4211689c2de0f9ba09f6f78c3004b6906
AlptugKizilay <alptugkizilay@gmail.com>
2023-04-26 02:17:02 +0300

    validation for data

--- queries.sql
+++ queries.sql
@@ -1,17 +1,25 @@
 -- Veritabanı Sorguları
 
 -- Posta kodu 1010 olan tüm müşterileri bulun
+SELECT * FROM Customers WHERE PostalCode=1010;
 
 -- id'si 11 olan tedarikçinin telefon numarasını bulun
+SELECT Phone FROM [Suppliers] Where SupplierId = 11;
 
 -- Verilen ilk 10 siparişi, sipariş tarihine göre azalan şekilde listeleyin
+SELECT * FROM [Orders] order by OrderDate desc limit 10;
 
 -- Londra, Madrid veya Brezilya'da yaşayan tüm müşterileri bulun
+SELECT * FROM [Customers] where City IN ('London','Madrid') or Country like 'Brazil';
 
 -- "The Shire" için bir müşteri kaydı ekleyin, ilgili kişi adı "Bilbo Baggins", adres - "Bag End" içinde "1 Hobbit-Hole", posta kodu "111" ve ülke "Middle Earth"
-
+INSERT INTO [Customers] (CustomerName,ContactName,Address,City,PostalCode,Country)
+VALUES ("The Shire","Bilbo Baggins","1 Hobbit-Hole","Bag End","111","Middle Earth");
 -- Posta kodu "11122" olarak değişecek şekilde Bilbo Baggins kaydını güncelleyin
-
+UPDATE [Customers]
+SET PostalCode = "111222"
+WHERE CustomerID = 92;
 -- (Zorlayıcı Görev) Müşteriler tablosunda kaç farklı şehrin saklandığını keşfetmek için bir sorgu bulun. Tekrarlar çift sayılmamalıdır
-
+SELECT count(distinct City) FROM [Customers];
 -- (Zorlayıcı Görev) 20 karakterden uzun adları olan tüm tedarikçileri bulun. Adın uzunluğunu almak için "length(SupplierName)" kullanabilirsiniz.
+SELECT SupplierName FROM [Suppliers] where length(SupplierName)>20;
